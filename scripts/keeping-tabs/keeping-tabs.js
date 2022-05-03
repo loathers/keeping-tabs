0d813b161d85662a8d5cd2b2380a5e682439e525
Patrick Stalcup <legomaniac0117@gmail.com>
2022-05-03 12:03:45 -0400

    Structure for release branch

---
+++ scripts/keeping-tabs/keeping-tabs.js
@@ -0,0 +1,398 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "main": () => (/* binding */ main)
+});
+
+;// CONCATENATED MODULE: external "kolmafia"
+const external_kolmafia_namespaceObject = require("kolmafia");
+;// CONCATENATED MODULE: ./node_modules/libram/dist/template-string.js
+
+
+var concatTemplateString = function concatTemplateString(literals) {
+  for (var _len = arguments.length, placeholders = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    placeholders[_key - 1] = arguments[_key];
+  }
+
+  return literals.reduce((acc, literal, i) => acc + literal + (placeholders[i] || ""), "");
+};
+
+var createSingleConstant = Type => function (literals) {
+  for (var _len2 = arguments.length, placeholders = new Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+    placeholders[_key2 - 1] = arguments[_key2];
+  }
+
+  var input = concatTemplateString.apply(void 0, [literals].concat(placeholders));
+  return Type.get(input);
+};
+
+var createPluralConstant = Type => function (literals) {
+  for (var _len3 = arguments.length, placeholders = new Array(_len3 > 1 ? _len3 - 1 : 0), _key3 = 1; _key3 < _len3; _key3++) {
+    placeholders[_key3 - 1] = arguments[_key3];
+  }
+
+  var input = concatTemplateString.apply(void 0, [literals].concat(placeholders));
+
+  if (input === "") {
+    return Type.all();
+  }
+
+  return Type.get(input.split(/\s*,\s*/));
+};
+/**
+ * A Bounty specified by name.
+ *
+ * @category In-game constant
+ */
+
+
+var $bounty = createSingleConstant(external_kolmafia_namespaceObject.Bounty);
+/**
+ * A list of Bounties specified by a comma-separated list of names.
+ * For a list of all possible Bounties, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $bounties = createPluralConstant(external_kolmafia_namespaceObject.Bounty);
+/**
+ * A Class specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $class = createSingleConstant(external_kolmafia_namespaceObject.Class);
+/**
+ * A list of Classes specified by a comma-separated list of names.
+ * For a list of all possible Classes, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $classes = createPluralConstant(external_kolmafia_namespaceObject.Class);
+/**
+ * A Coinmaster specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $coinmaster = createSingleConstant(external_kolmafia_namespaceObject.Coinmaster);
+/**
+ * A list of Coinmasters specified by a comma-separated list of names.
+ * For a list of all possible Coinmasters, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $coinmasters = createPluralConstant(external_kolmafia_namespaceObject.Coinmaster);
+/**
+ * An Effect specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $effect = createSingleConstant(external_kolmafia_namespaceObject.Effect);
+/**
+ * A list of Effects specified by a comma-separated list of names.
+ * For a list of all possible Effects, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $effects = createPluralConstant(external_kolmafia_namespaceObject.Effect);
+/**
+ * An Element specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $element = createSingleConstant(external_kolmafia_namespaceObject.Element);
+/**
+ * A list of Elements specified by a comma-separated list of names.
+ * For a list of all possible Elements, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $elements = createPluralConstant(external_kolmafia_namespaceObject.Element);
+/**
+ * A Familiar specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $familiar = createSingleConstant(external_kolmafia_namespaceObject.Familiar);
+/**
+ * A list of Familiars specified by a comma-separated list of names.
+ * For a list of all possible Familiars, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $familiars = createPluralConstant(external_kolmafia_namespaceObject.Familiar);
+/**
+ * An Item specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $item = createSingleConstant(external_kolmafia_namespaceObject.Item);
+/**
+ * A list of Items specified by a comma-separated list of names.
+ * For a list of all possible Items, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $items = createPluralConstant(external_kolmafia_namespaceObject.Item);
+/**
+ * A Location specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $location = createSingleConstant(external_kolmafia_namespaceObject.Location);
+/**
+ * A list of Locations specified by a comma-separated list of names.
+ * For a list of all possible Locations, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $locations = createPluralConstant(external_kolmafia_namespaceObject.Location);
+/**
+ * A Monster specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $monster = createSingleConstant(external_kolmafia_namespaceObject.Monster);
+/**
+ * A list of Monsters specified by a comma-separated list of names.
+ * For a list of all possible Monsters, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $monsters = createPluralConstant(external_kolmafia_namespaceObject.Monster);
+/**
+ * A Phylum specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $phylum = createSingleConstant(external_kolmafia_namespaceObject.Phylum);
+/**
+ * A list of Phyla specified by a comma-separated list of names.
+ * For a list of all possible Phyla, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $phyla = createPluralConstant(external_kolmafia_namespaceObject.Phylum);
+/**
+ * A Servant specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $servant = createSingleConstant(external_kolmafia_namespaceObject.Servant);
+/**
+ * A list of Servants specified by a comma-separated list of names.
+ * For a list of all possible Servants, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $servants = createPluralConstant(external_kolmafia_namespaceObject.Servant);
+/**
+ * A Skill specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $skill = createSingleConstant(external_kolmafia_namespaceObject.Skill);
+/**
+ * A list of Skills specified by a comma-separated list of names.
+ * For a list of all possible Skills, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $skills = createPluralConstant(external_kolmafia_namespaceObject.Skill);
+/**
+ * A Slot specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $slot = createSingleConstant(external_kolmafia_namespaceObject.Slot);
+/**
+ * A list of Slots specified by a comma-separated list of names.
+ * For a list of all possible Slots, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $slots = createPluralConstant(external_kolmafia_namespaceObject.Slot);
+/**
+ * A Stat specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $stat = createSingleConstant(external_kolmafia_namespaceObject.Stat);
+/**
+ * A list of Stats specified by a comma-separated list of names.
+ * For a list of all possible Stats, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $stats = createPluralConstant(external_kolmafia_namespaceObject.Stat);
+/**
+ * A Thrall specified by name.
+ *
+ * @category In-game constant
+ */
+
+var $thrall = createSingleConstant(external_kolmafia_namespaceObject.Thrall);
+/**
+ * A list of Thralls specified by a comma-separated list of names.
+ * For a list of all possible Thralls, leave the template string blank.
+ *
+ * @category In-game constant
+ */
+
+var $thralls = createPluralConstant(external_kolmafia_namespaceObject.Thrall);
+;// CONCATENATED MODULE: ./src/main.ts
+var _templateObject;
+
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+
+
+var tabLookup = {
+  "mall this": 2,
+  "display this": 3,
+  "use this": 4,
+  "autosell this": 5
+};
+var actions = {
+  "mall this": item => (0,external_kolmafia_namespaceObject.putShop)(0, 0, item),
+  "display this": item => (0,external_kolmafia_namespaceObject.putDisplay)((0,external_kolmafia_namespaceObject.itemAmount)(item), item),
+  "use this": item => (0,external_kolmafia_namespaceObject.use)((0,external_kolmafia_namespaceObject.itemAmount)(item), item),
+  "autosell this": item => (0,external_kolmafia_namespaceObject.autosell)((0,external_kolmafia_namespaceObject.itemAmount)(item), item)
+};
+
+function getItemsInTab(title) {
+  (0,external_kolmafia_namespaceObject.print)("Matching tab ".concat(title));
+  var tab = (0,external_kolmafia_namespaceObject.visitUrl)("inventory.php?which=f".concat(tabLookup[title]));
+  var regexp = /ic(\d+)/g;
+  var match;
+  var items = [];
+
+  while ((match = regexp.exec(tab)) !== null) {
+    var _item = (0,external_kolmafia_namespaceObject.toItem)((0,external_kolmafia_namespaceObject.toInt)(match[1]));
+
+    items.push(_item);
+  }
+
+  return items;
+}
+
+var fixedPrices = new Map([[$item(_templateObject || (_templateObject = _taggedTemplateLiteral(["pocket wish"]))), 49995]]);
+
+function shopItems() {
+  return Object.keys((0,external_kolmafia_namespaceObject.getShop)()).map(itemStr => (0,external_kolmafia_namespaceObject.toItem)(itemStr));
+}
+
+function main() {
+  var args = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "use mall autosell display";
+  var commandLookup = new Map([["mall", "mall this"], ["display", "display this"], ["use", "use this"], ["autosell", "autosell this"]]);
+  var commands = args.split(" ").map(command => commandLookup.get(command));
+
+  var _iterator = _createForOfIteratorHelper(commands),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var command = _step.value;
+
+      if (command) {
+        (0,external_kolmafia_namespaceObject.print)("Executing ".concat(command), "yellow");
+        getItemsInTab(command).forEach(actions[command]);
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+
+  if (commands.includes("mall this")) {
+    shopItems().forEach(item => {
+      var fixedPrice = fixedPrices.get(item);
+
+      if (fixedPrice) {
+        (0,external_kolmafia_namespaceObject.repriceShop)(fixedPrice, item);
+      }
+    });
+    (0,external_kolmafia_namespaceObject.cliExecute)("reprice");
+    var maxPriceItems = shopItems().filter(i => (0,external_kolmafia_namespaceObject.shopPrice)(i) === 999999999);
+
+    if (maxPriceItems.length > 0) {
+      (0,external_kolmafia_namespaceObject.print)("Some Items are Still at Max Price", "red");
+      maxPriceItems.forEach(i => (0,external_kolmafia_namespaceObject.print)("* ".concat(i), "red"));
+    }
+  }
+}
+var __webpack_export_target__ = exports;
+for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
+if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+/******/ })()
+;
